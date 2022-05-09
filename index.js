bc8e202431474bfbcb5812944150d351880641af
Tate Mitchell <htatemitchell@gmail.com>
2022-05-08 22:39:35 -0600

    deploy attempt

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 let data = require("./data/gamestores.js");
 const searchUtils = require("./utils/searchUtils.js");
 const app = express();
-const PORT = 8081;
+const PORT = process.env.PORT || 8081;
 
 app.use(cors());
 app.use(express.json());
